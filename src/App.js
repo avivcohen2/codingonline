a5e434d4c1d8bc62e0e4b5e78de8283ad69abfa9
avivcohen2 <78810473+avivcohen2@users.noreply.github.com>
2024-02-22 17:11:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import {
 } from "react-router-dom";
 import HomePage from './components/HomePage/HomePage';
 import CodeBlockPage from './components/CodeBlockPage/CodeBlockPage';
-const fetch = require('node-fetch')
 
 class App extends Component {
     constructor(props) {
