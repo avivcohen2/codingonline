7ccd4a5681a0913b167fa49e708aa2009ea0f27f
avivcohen2 <78810473+avivcohen2@users.noreply.github.com>
2024-02-23 13:18:03 +0200

    Update CodeBlockPage.js

--- src/components/CodeBlockPage/CodeBlockPage.js
+++ src/components/CodeBlockPage/CodeBlockPage.js
@@ -19,7 +19,7 @@ const CodeBlockPage = ({ codeblocks }) => {
         console.log('new connection')
         hljs.highlightElement(textareaRef.current);
 
-        const newSocket = io('http://localhost:3000');
+        const newSocket = io('https://codingonline-rsss.onrender.com');
         setSocket(newSocket);
 
         return () => {
